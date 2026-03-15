30e0d4e92a644d53f592eca736242f045c999247
MatthewKing2 <matthewjesseking@gmail.com>
2026-03-14 21:39:46 -0500

    Drawing a Triangle - Setup - Base Code

--- testing_vulkan/src/main.cpp
+++ testing_vulkan/src/main.cpp
@@ -1,35 +1,61 @@
-#define GLFW_INCLUDE_VULKAN
+// #include <vulkan/vulkan.h>  //Include the Vulkan header which provides the functions, structures and enumerations.  
+#define GLFW_INCLUDE_VULKAN     // Replace vulkan header with the GLFW one (this automaticlaly loads the vulkan one)
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
 
-int main() {
-    glfwInit();
+class HelloTriangleApplication {
+private:
+    GLFWwindow* window;             // Stores pointer to the window we writing stuff to
+    const uint32_t WIDTH = 800;     // Window Width
+    const uint32_t HEIGHT = 600;    // Window Height
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    std::cout << extensionCount << " extensions supported\n";
-
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+public:
+    void run() {
+        initWindow();   // Initalized the window that we write to
+        initVulkan();
+        mainLoop();
+        cleanup();
     }
 
-    glfwDestroyWindow(window);
+private:
+    void initWindow() {
+        glfwInit();
 
-    glfwTerminate();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);   // Init the GLFW library. Tell it not to use OpenGL API (we doing vulkan)
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);     // Diabling resizable windows b/c they require extra work
 
-    return 0;
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);    // Args = Width, Height, Title, Monitor*, OpenGL*. 
+    }
+
+    void initVulkan() {
+
+    }
+
+    // This is the main (forever) loop of the HellowTriangleApplication (render frames)
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {    // while you should not close the window: 
+            glfwPollEvents();                       // polls for events, like clicking the X button
+        }
+    }
+
+    void cleanup() {
+        glfwDestroyWindow(window);      // destory the window
+        glfwTerminate();                // turn off glfw 
+    }
+};
+
+int main() {
+    HelloTriangleApplication app;
+
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
